57af45c267075c6af3897c43c8f9546a436780f4
flavour-flex <chris@crxs.de>
2025-02-28 21:24:27 +0100

    menu

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -148,8 +148,8 @@ export default function Layout({ children }: LayoutProps) {
                       {navigation.map((item) => (
                         <Fragment key={item.name}>
                           {!item.children ? (
-                            <Link
-                              href={item.href || '#'}
+                            <button
+                              onClick={() => handleNavigation(item.href)}
                               className={classNames(
                                 router.pathname === item.href
                                   ? 'bg-[#023770]/10 text-white'
@@ -158,7 +158,7 @@ export default function Layout({ children }: LayoutProps) {
                               )}
                             >
                               {item.name}
-                            </Link>
+                            </button>
                           ) : (
                             <Menu as="div" className="relative">
                               <Menu.Button
@@ -198,18 +198,18 @@ export default function Layout({ children }: LayoutProps) {
                                         {child.href ? (
                                           <Menu.Item>
                                             {({ active }) => (
-                                              <Link
-                                                href={child.href}
+                                              <button
+                                                onClick={() => handleNavigation(child.href)}
                                                 className={classNames(
                                                   active ? 'bg-gray-100' : '',
-                                                  'block px-4 py-2 text-sm text-gray-700'
+                                                  'block w-full text-left px-4 py-2 text-sm text-gray-700'
                                                 )}
                                               >
                                                 <span className="flex items-center">
                                                   <child.icon className="mr-3 h-5 w-5 text-gray-400" aria-hidden="true" />
                                                   {child.name}
                                                 </span>
-                                              </Link>
+                                              </button>
                                             )}
                                           </Menu.Item>
                                         ) : (
