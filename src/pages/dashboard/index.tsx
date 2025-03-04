4cab735894c3018ea83137a665b40919e3bfa36f
flavour-flex <chris@crxs.de>
2025-03-04 19:14:01 +0100

    test

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -370,7 +370,7 @@ export default function DashboardPage() {
           <div className="bg-white shadow sm:rounded-lg">
             <div className="px-4 py-5 sm:p-6">
               <h3 className="text-base font-semibold leading-6 text-gray-900">
-                Letzte Wareneingänge
+                Letzte Wareneingängee
               </h3>
               <div className="mt-4 flow-root">
                 <table className="min-w-full divide-y divide-gray-300">
